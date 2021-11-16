028a30dfecd07b7db079e10bbe5a284db8a77253
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-16 18:53:06 -0300

    :recycle: changed login page

--- my-personal-wallet/src/pages/components/LoginForm.jsx
+++ my-personal-wallet/src/pages/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LoginForm = function () {
   return (
@@ -8,19 +9,23 @@ const LoginForm = function () {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            Well never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
+          <Form.Text className="text-muted">
+            Well never share your password with anyone else.
+          </Form.Text>
         </Form.Group>
+
+        <Link to="/create">Don&apos;t have an account? Sign up here.</Link>
+
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+
+        <Button variant="primary" type="button">
           Submit
         </Button>
       </Form>
