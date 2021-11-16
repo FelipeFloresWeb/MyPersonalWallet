fc1443d3480c96772aba4793d5a0363775b345cd
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-16 18:11:04 -0300

    :recycle: configured eslint rc

--- my-personal-wallet/src/App.js
+++ my-personal-wallet/src/App.js
@@ -1,9 +1,10 @@
-function App() {
+import React from 'react';
+
+const App = function () {
   return (
     <div>
-     <h2>My Personal Wallet</h2>
+      <h2>My Personal Wallet</h2>
     </div>
   );
-}
-
+};
 export default App;
