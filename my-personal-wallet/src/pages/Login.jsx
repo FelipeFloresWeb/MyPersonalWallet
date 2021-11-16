cb8f2837b3bdd970d64dc72a5801b7043d8b3e32
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-16 18:26:42 -0300

    add login page

---
+++ my-personal-wallet/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable func-names */
+import React from 'react';
+
+const Login = function () {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+};
+
+export default Login;
