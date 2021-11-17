bf2926f84364a73834f04130a73940afe455f02b
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-17 00:13:41 -0300

    added a Mock Api

---
+++ my-personal-wallet/src/services/Api.js
@@ -0,0 +1,30 @@
+const getUser = ({ email, password }) => new Promise((resolve, reject) => {
+  if (email && password) {
+    resolve({
+      status: 200,
+      user: {
+        name: 'Pedro', email: 'pedro@pedro.com', id: '1', balance: 2000,
+      },
+    });
+  } else {
+    reject(new Error('User Not Found'));
+  }
+});
+
+const createUser = ({ name, email, password }) => new Promise((resolve, reject) => {
+  if (name && email && password) {
+    resolve({
+      status: 201,
+      token: 'sad65f1as5df1as65df1a5s6df15a6sd1fas65df165asd1fa6sd1fa6sd516sd',
+    });
+  } else {
+    reject(new Error('Invalid Fields'));
+  }
+});
+
+const ApiMock = {
+  getUser,
+  createUser,
+};
+
+export default ApiMock;
