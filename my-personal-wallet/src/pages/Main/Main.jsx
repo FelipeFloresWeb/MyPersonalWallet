aa64ce82ac90e2bd1b35c9e9f7d2620ba5834616
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-18 14:23:38 -0300

    added div keeping the text centered and can be removed.

--- my-personal-wallet/src/pages/Main/Main.jsx
+++ my-personal-wallet/src/pages/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { CloseButton } from 'react-bootstrap';
 
 import Header from './Components/Header';
 import Wallet from './Components/Wallet';
@@ -9,11 +10,21 @@ import Currencies from './Components/Currencies';
 const Main = function (props) {
   const { name } = props;
   const [handlePages, setHandlePages] = useState('');
+  const [togleText, setTogleText] = useState(false);
 
   return (
     <>
       <Header setHandlePages={setHandlePages} handlePages={handlePages} />
-      <h3>{`Welcome ${name} Select 'Wallet' to view your wallet or 'Currencies' to view market offers`}</h3>
+      <div hidden={togleText} className="container overflow-hidden">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="p-3 border bg-light">
+              <CloseButton onClick={() => setTogleText(!togleText)} aria-label="Hide" />
+              <h3>Select Wallet to view your wallet or Currencies to view market offers</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       {handlePages === 'wallet' ? <Wallet /> : ''}
       {handlePages === 'currencies' ? <Currencies /> : ''}
     </>
