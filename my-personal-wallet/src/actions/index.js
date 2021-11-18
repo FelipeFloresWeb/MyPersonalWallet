be10779eb3f9295fd999cb0aa39a8a6eed9bc8db
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-18 13:23:16 -0300

    :sparkles:added: 2 new actions: buying and selling currencies

--- my-personal-wallet/src/actions/index.js
+++ my-personal-wallet/src/actions/index.js
@@ -8,6 +8,28 @@ export const USER_LOGIN_SUCESS = 'USER_LOGIN_SUCESS';
 export const USER_LOGIN_ERROR = 'USER_LOGIN_ERROR';
 export const LOAD_CURRENCIES = 'LOAD_CURRENCIES';
 export const ADD_EXCHANGE_RATES = 'ADD_EXCHANGE_RATES';
+export const ADD_USER_SPENT = 'ADD_USER_SPENT';
+export const ADD_USER_SPENT_ERROR = 'ADD_USER_SPENT_ERROR';
+export const ADD_USER_GAIN_ERROR = 'ADD_USER_GAIN_ERROR';
+export const ADD_USER_GAIN = 'ADD_USER_GAIN';
+
+export const addUserGainError = () => ({
+  type: ADD_USER_GAIN_ERROR,
+});
+
+export const addUserSpentError = () => ({
+  type: ADD_USER_SPENT_ERROR,
+});
+
+export const addUserGain = (payload) => ({
+  type: ADD_USER_GAIN,
+  payload,
+});
+
+export const addUserSpent = (payload) => ({
+  type: ADD_USER_SPENT,
+  payload,
+});
 
 export const userCreateSucess = (payload) => ({
   type: USER_CREATE_SUCESS,
@@ -40,32 +62,69 @@ export const addExchangeRates = (payload) => ({
   payload,
 });
 
-export const mockAPIThunkCreate = (payload) => async (dispatch) => {
-  dispatch(userLogin());
-
-  const { status, user: { name, email, balance } } = await mockApi.createUser(
-    { name: payload.name, email: payload.email, password: payload.password },
+// Sell Currencies
+export const mockAPIThunkGain = (payload) => async (dispatch) => {
+  if (payload.balance === 'NaN') return dispatch(addUserGainError());
+  const {
+    status, user: { balance, wallets },
+  } = await mockApi.addSpentUser(
+    { email: payload.email, wallets: payload.wallets, balance: payload.balance },
   );
 
-  if (status !== 201) return dispatch(userCreateError());
+  if (status !== 201) return dispatch(addUserGainError());
 
-  return dispatch(userCreateSucess({ email, name, balance }));
+  return dispatch(addUserGain({ balance, wallets }));
 };
 
-export const mockAPIThunk = (payload) => async (dispatch) => {
-  dispatch(userLogin());
+// Buy Currencies
+export const mockAPIThunkSpent = (payload) => async (dispatch) => {
+  if (payload.balance === 'NaN') return dispatch(addUserSpentError());
+  const {
+    status, user: { balance, wallets },
+  } = await mockApi.addSpentUser(
+    { email: payload.email, wallets: payload.wallets, balance: payload.balance },
+  );
 
+  if (status !== 201) return dispatch(addUserSpentError());
+
+  return dispatch(addUserSpent({ balance, wallets }));
+};
+
+// Create User
+export const mockAPIThunkCreate = (payload) => async (dispatch) => {
+  dispatch(userLogin());
   const {
     status,
     user: {
-      balance, email, name,
+      balance, email, name, wallets,
+    },
+  } = await mockApi.createUser(
+    { name: payload.name, email: payload.email, password: payload.password },
+  );
+  if (status !== 200) return dispatch(userLoginError());
+
+  return dispatch(userLoginSucess({
+    name, email, balance, wallets,
+  }));
+};
+
+// Login User
+export const mockAPIThunk = (payload) => async (dispatch) => {
+  dispatch(userLogin());
+  const {
+    status,
+    user: {
+      balance, email, name, wallets,
     },
   } = await mockApi.getUser({ email: payload.email, password: payload.password });
   if (status !== 200) return dispatch(userLoginError());
 
-  return dispatch(userLoginSucess({ name, email, balance }));
+  return dispatch(userLoginSucess({
+    name, email, balance, wallets,
+  }));
 };
 
+// Load Currencies
 export const currencyAPIThunk = () => async (dispatch) => {
   dispatch(LoadCurrencies());
 
