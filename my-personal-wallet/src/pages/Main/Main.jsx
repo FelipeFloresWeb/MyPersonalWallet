6fa81e54009143b6743221a8a0aacd1ac2679249
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-18 23:22:14 -0300

    :recycle: remove name props, add data test-ids for app tests

--- my-personal-wallet/src/pages/Main/Main.jsx
+++ my-personal-wallet/src/pages/Main/Main.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { CloseButton } from 'react-bootstrap';
 
@@ -7,19 +6,18 @@ import Header from './Components/Header';
 import Wallet from './Components/Wallet';
 import Currencies from './Components/Currencies';
 
-const Main = function (props) {
-  const { name } = props;
+const Main = function () {
   const [handlePages, setHandlePages] = useState('');
   const [togleText, setTogleText] = useState(false);
 
   return (
     <>
       <Header setHandlePages={setHandlePages} handlePages={handlePages} />
-      <div hidden={togleText} className="container overflow-hidden">
+      <div data-testid="close-div" hidden={togleText} className="container overflow-hidden">
         <div className="row gx-5">
           <div className="col">
             <div className="p-3 border bg-light">
-              <CloseButton onClick={() => setTogleText(!togleText)} aria-label="Hide" />
+              <CloseButton data-testid="close-button" onClick={() => setTogleText(!togleText)} aria-label="Hide" />
               <h3>Select Wallet to view your wallet or Currencies to view market offers</h3>
             </div>
           </div>
@@ -31,12 +29,4 @@ const Main = function (props) {
   );
 };
 
-const mapStateToProps = ({ user: { name } }) => ({
-  name,
-});
-
-Main.propTypes = {
-  name: PropTypes.string,
-}.isRequired;
-
-export default connect(mapStateToProps)(Main);
+export default connect()(Main);
